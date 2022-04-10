87615925f6747a3577679054a2065ae1dddfbbfe
faridnizam <faridsaja12@gmail.com>
2022-04-10 06:47:53 +0000

    Deploying to pages from @ faridnizam/card-profile@1e1e785380c81b926e0044530a3fd70dd0d3c538 🚀

---
+++ _next/static/QxfhfSq5Urk_iRX0qU9y4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/569-8b1410c135677432.js","static/chunks/pages/index-6b0db5743e31fada.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
